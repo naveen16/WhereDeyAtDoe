a84b98167e360e1713b723e40ddcb0257b7273c0
rajvanshyr <rajvanshyr@utexas.edu>
2017-04-19 16:53:54 -0500

    added instructions

---
+++ app/src/main/java/naveen16/wheredeyatdoe/InstructionsActivity.java
@@ -0,0 +1,14 @@
+package naveen16.wheredeyatdoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InstructionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
+
+    }
+}
